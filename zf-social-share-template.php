f30eff2ffd22484d2018f008c53c9c4533fc946f
Amanda Koivisto <amanda.koivisto@zeelandfamily.fi>
2017-07-04 08:08:14 +0300

    Added unique classes for social links items

--- zf-social-share-template.php
+++ zf-social-share-template.php
@@ -1,7 +1,7 @@
 <div class="zf-social-links">
 
 	<ul class="social-links__list">
-	<li class="social-links__item">
+	<li class="social-links__item--facebook">
 		<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>&amp;t=<?php echo urlencode( esc_attr( get_the_title() ) ); ?>" target="_blank" rel="noopener noreferrer" class="social-links__link">
 		<span class="social-links__text">
 
@@ -15,7 +15,7 @@
 		</span>
 	  </a>
 	</li>
-	<li class="social-links__item">
+	<li class="social-links__item--twitter">
 		<a href="https://twitter.com/share?text=<?php the_title(); ?>&amp;url=<?php echo urlencode( esc_url( get_permalink( get_the_ID() ) ) ); ?>" target="_blank" rel="noopener noreferrer" class="social-links__link">
 		<span class="social-links__text">
 
@@ -29,7 +29,7 @@
 		</span>
 	  </a>
 	</li>
-	<li class="social-links__item">
+	<li class="social-links__item--linkedin">
 	  <a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo urlencode( esc_url( get_permalink( get_the_ID() ) ) ); ?>&title=<?php the_title(); ?>" target="_blank" rel="noopener noreferrer" class="social-links__link">
 		<span class="social-links__text">
 
